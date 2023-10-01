88340c62fd83f7b7f981b18bc9b3afc7d75e0531
mine2424 <minewest065524@gmail.com>
2023-10-01 17:29:59 +0900

    fix: tap bug

--- lib/presentation/screen/iat_test_screen/iat_test_screen.dart
+++ lib/presentation/screen/iat_test_screen/iat_test_screen.dart
@@ -16,13 +16,13 @@ class IATTestScreen extends HookWidget {
     '一日の終わりには、自分の仕事に満足していることが多いですか？',
     '週末に仕事のことを考えることはよくありますか？',
     '仕事のストレスを感じることは日常的ですか？',
-    // '仕事の量は適切だと感じますか？',
-    // '仕事の締め切りに追われることが多いですか？',
-    // '仕事中に適切に休息をとることができていますか？',
-    // '上司や同僚とのコミュニケーションを、ストレスを感じることなくスムーズに取ることができていますか？',
-    // '仕事のミスに対して、過度なプレッシャーを感じますか？',
-    // '仕事に対する自信はありますか？',
-    // '仕事の目標や期待値が明確ですか？',
+    '仕事の量は適切だと感じますか？',
+    '仕事の締め切りに追われることが多いですか？',
+    '仕事中に適切に休息をとることができていますか？',
+    '上司や同僚とのコミュニケーションを、ストレスを感じることなくスムーズに取ることができていますか？',
+    '仕事のミスに対して、過度なプレッシャーを感じますか？',
+    '仕事に対する自信はありますか？',
+    '仕事の目標や期待値が明確ですか？',
     '',
   ];
 
@@ -31,6 +31,8 @@ class IATTestScreen extends HookWidget {
 
   final stopWatch = Stopwatch()..start();
 
+  final carouselController = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     final elapsedMilliseconds = useState<double>(0);
@@ -38,12 +40,8 @@ class IATTestScreen extends HookWidget {
 
     final evaluateIAPSum = useState<double>(0);
 
-    final leftTextKeyList =
-        List.generate(questions.length, (index) => GlobalKey());
-    final rightTextKeyList =
-        List.generate(questions.length, (index) => GlobalKey());
-
-    final carouselController = CarouselController();
+    final leftTextKeyList = List.generate(questions.length, (index) => GlobalKey());
+    final rightTextKeyList = List.generate(questions.length, (index) => GlobalKey());
 
     useEffect(
       () {
@@ -62,8 +60,7 @@ class IATTestScreen extends HookWidget {
 
       final elapsed = stopWatch.elapsed;
       // ignore: lines_longer_than_80_chars
-      final elapsedByStop =
-          double.parse('${elapsed.inSeconds}.${elapsed.inMilliseconds}');
+      final elapsedByStop = double.parse('${elapsed.inSeconds}.${elapsed.inMilliseconds}');
       debugPrint('elapsedSeconds: ${elapsedMilliseconds.value}');
 
       // A: 回答の+,-の位置の評価（横軸のみ）
@@ -76,10 +73,8 @@ class IATTestScreen extends HookWidget {
       debugPrint('widthPosition: $widthPosition');
 
       // B: tapの位置の評価
-      final leftTextRenderBox =
-          leftSideTextKey.currentContext!.findRenderObject()! as RenderBox;
-      final rightTextRenderBox =
-          rightSideTextKey.currentContext!.findRenderObject()! as RenderBox;
+      final leftTextRenderBox = leftSideTextKey.currentContext!.findRenderObject()! as RenderBox;
+      final rightTextRenderBox = rightSideTextKey.currentContext!.findRenderObject()! as RenderBox;
 
       // widthの半分より左側を選択した場合は左側のテキストの座標を取得
       // 右側を選択した場合は右側のテキストの座標を取得
@@ -100,8 +95,7 @@ class IATTestScreen extends HookWidget {
       final fx = textPosition.dx;
       final fy = textPosition.dy;
 
-      final tapDistance =
-          sqrt(pow(fx - regulatedDx, 2) + pow(fy - regulatedDy, 2));
+      final tapDistance = sqrt(pow(fx - regulatedDx, 2) + pow(fy - regulatedDy, 2));
 
       debugPrint('tap distance: $tapDistance');
 
@@ -126,6 +120,7 @@ class IATTestScreen extends HookWidget {
         ..value += tapDistance / 1000;
 
       // naxt page
+      carouselController.nextPage();
     }
 
     return Scaffold(
@@ -192,26 +187,19 @@ class IATTestScreen extends HookWidget {
                             children: [
                               Text(
                                 'そう思わない',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleLarge!
-                                    .copyWith(fontWeight: FontWeight.bold),
+                                style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
                                 key: leftTextKeyList[i],
                               ),
                               Text(
                                 'そう思う',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleLarge!
-                                    .copyWith(fontWeight: FontWeight.bold),
+                                style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
                                 key: rightTextKeyList[i],
                               ),
                             ],
                           ),
                         ),
                         const Gap(60),
-                        LottieGenImage(Assets.lottie.greentimer.path)
-                            .lottie(width: 300),
+                        LottieGenImage(Assets.lottie.greentimer.path).lottie(width: 300),
                       ],
                     );
             }).toList(),
